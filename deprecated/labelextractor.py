b2608f36079bf7acfdbb8fb04c870acef0d55812
hampus17 <hampus.a02@gmail.com>
2021-01-22 14:24:34 +0100

    Clean up and added lanenet

--- deprecated/labelextractor.py
+++ deprecated/labelextractor.py
@@ -1,11 +1,21 @@
 import json
 import os
 import sys
+import pandas as pd
 
 p_data = {
-    'points': [
+    "label": {
+        
+    }, 
+    "xy_points": {
+        
+    },
+    "num_of_points": {
 
-    ]
+    },
+    "id": {
+
+    }
 }
 
 def extract(folder_path, proc_labelset_path, labelset_prefix):
@@ -18,29 +28,37 @@ def extract(folder_path, proc_labelset_path, labelset_prefix):
             labelset_name (string): This is the prefix for the output labelsets
     '''
     
-    
+    frame_count = 0
     for file in os.listdir(folder_path):
         if file.endswith(".json"):
-            write_to = proc_labelset_path + "{}_frame.json".format(labelset_prefix)
+            write_to = proc_labelset_path + "{}_frame{}".format(labelset_prefix, frame_count)
             filepath = os.path.join(folder_path, file)
             with open(filepath) as json_file:
-                i = 1
+                id = 0
+                
                 data = json.load(json_file)
                 for obj_key in data['objects']:
                     points_key = obj_key['points']
-                    class_key = obj_key['classTitle']
+                    label = obj_key['classTitle']
                     coords = []
+                    points = 0
                     for point in points_key['exterior']:
-                        coords.append(point) 
-                    p_data['points'].append({
-                        'label' : class_key,
-                        'lane_points' : coords
-                        })
-                    i = i + 1
+                        coords.extend(point)
+                        points += 1        
+                    p_data["label"][id] = label
+                    p_data["xy_points"][id] = coords
+                    p_data["num_of_points"][id] = (points / 2)
+                    p_data["id"][id] = (id + 1)
+                    id += 1
 
-            json_str = json.dumps(p_data, indent=4)
-            with open(write_to, 'w') as outfile:
-                outfile.write(json_str)
+            json_file = json.dumps(p_data, indent=4)
+            json_df = pd.read_json(json_file)
+            csv_file = json_df.to_csv()
+            with open("{}.json".format(write_to), 'w') as outfile:
+                outfile.write(json_file)
+            with open("{}.csv".format(write_to), 'w') as outfile:
+                outfile.write(csv_file)
+            frame_count = frame_count + 1
             continue
         else:
             print("Warning: Skipped {} (Not a JSON file)".format(file))
